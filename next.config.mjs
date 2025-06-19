d121f35c1eff62fb5b1899cd6ef6577621905423
Qiang Zhao <mattisonchao@apache.org>
2025-06-19 11:27:42 +0800

    disable setup pages

--- next.config.mjs
+++ next.config.mjs
@@ -9,5 +9,9 @@ const withNextra = nextra({
 })
 
 export default withNextra({
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true // mandatory, otherwise won't export
+  }
 })
