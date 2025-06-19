d4658f8923f598a7574ec2f534c0ee9abb171b06
Qiang Zhao <mattisonchao@apache.org>
2025-06-19 11:38:17 +0800

    change configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,17 @@
 import nextra from 'nextra'
 
-const withNextra = nextra({
+const config = nextra({
   latex: true,
   search: {
     codeblocks: false
   },
   contentDirBasePath: '/docs'
-})
-
-export default withNextra({
-  reactStrictMode: true,
+})({
   output: 'export',
   basePath: process.env.PAGES_BASE_PATH,
   images: {
     unoptimized: true // mandatory, otherwise won't export
   }
 })
+
+export default config
