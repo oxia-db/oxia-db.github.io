8f56acfe1dc31838c3e1cc36b59e6bacc5332874
Qiang Zhao <mattisonchao@apache.org>
2025-06-19 11:20:50 +0800

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,9 @@
 import nextra from 'nextra'
 
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true // mandatory, otherwise won't export
-  },
   plugins: {
     "@tailwindcss/postcss": {},
   }
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: "build"
 }
 const withNextra = nextra({
   latex: true,
