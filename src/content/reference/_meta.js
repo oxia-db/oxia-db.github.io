4f38361d3a3fbe789b32b993526391ba5397c9d3
mattisonchao <mattisonchao@gmail.com>
2026-05-18 12:30:55 +0800

    docs: expose draft admin API reference

---
+++ src/content/reference/_meta.js
@@ -0,0 +1,17 @@
+export default {
+  cli: {
+    title: 'CLI',
+  },
+  config: {
+    title: 'Configuration',
+  },
+  grpc: {
+    title: 'gRPC API',
+  },
+  'admin-api': {
+    title: 'Admin API (Draft)',
+  },
+  metrics: {
+    title: 'Metrics',
+  },
+}
